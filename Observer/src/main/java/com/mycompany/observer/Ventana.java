d444558402b1631a23fb8707fefd91b32e4c8bfc
Juan Gallo <jcgallo@espol.edu.ec>
2021-10-31 19:41:21 -0500

    Creacion de ventana

---
+++ Observer/src/main/java/com/mycompany/observer/Ventana.java
@@ -0,0 +1,24 @@
+package com.mycompany.observer;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+public class Ventana {
+
+    @FXML
+    private VBox btnAzul;
+    @FXML
+    private Button btnAmarillo;
+    @FXML
+    private Button BtonAzul;
+    @FXML
+    private Button btnRojo;
+
+    
+    @FXML
+    private void CambiarColor(String color){
+        
+    }
+}
